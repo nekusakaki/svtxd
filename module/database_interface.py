f7bed6d82be95950f8b4115cc7c71b3a65fb89c9
nekusakaki <64486795+nekusakaki@users.noreply.github.com>
2020-05-01 14:31:43 -0400

    created database interface

---
+++ module/database_interface.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+class DatabaseInterface:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls, *args, **kwargs)
+
+        return cls._instance
+
+    def __init__(self):
+        self._conn = sqlite3.connect('card_database.db')
+        self._c = self._conn.cursor()
+
+    def get_card(self, card_id):
+        self._c.execute('SELECT * FROM cards WHERE card_id=:card_id', {'card_id': card_id})
+        return self._c.fetchone()
+
+
+db = DatabaseInterface()
+print(db.get_card(111541020))
+
