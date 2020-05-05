64b55b4dde0c97eddd01ae487186d9e313d132c6
nekusakaki <64486795+nekusakaki@users.noreply.github.com>
2020-05-05 11:22:19 -0400

    chesstimer turned into a countdown timer

--- module/chess timer.py
+++ module/chess timer.py
@@ -1,101 +1,44 @@
-import ui, time, notification, sound, sys
 
-view = ui.View()
-view.size_to_fit()
-view.background_color = 'white'
-view.flex = 'WH'
-
-time1 = 0
-time2 = 1
-time3 = 0
-time4 = 0
-time5 = 1
-time6 = 0
-tap1 == False
-tap2 == False
-
-player1button = ui.Button()
-player1button.border_color = '#198054#'
-player1button.border_width = 5
-player1button.frame = (90, 30, 200, 75)
-player1button.title = str(time1) + ':' + str(time2) + str(time3)
-
-player2button = ui.Button()
-player2button.border_color = '#198054#'
-player2button.border_width = 5
-player2button.frame = (90, 530, 200, 75)
-player2button.title = str(time1) + ':' + str(time2) + str(time3)
+from tkinter import *
 
 
-def player1tap(sender):
-    tap1 == True
-    tap2 == False
+t = 0
 
 
-def player2tap(sender):
-    tap1 == False
-    tap2 == True
+def set_timer():
+    global t
+    t = t+int(e1.get())
+    return t
 
 
-@ui.in_background
-def timer1(sender):
-    global time1
-    global time2
-    global time3
-    global tap1
-    global tap2
-
-    if tap1 == False:
-        if time6 == 0 & (time5 > 0 | time4 > 0):
-            time6 = 9
-            if time5 == 0:
-                time4 -= 1
-                time5 = 5
-            else:
-                time5 -= 1
-        else:
-            time6 -= 1
-            time.sleep(1)
-            player1button.title = str(time4) + ':' + str(time5) + str(time6)
-            timer2(sender)
-        if time4 == time5 == time6 == 0:
-            notification.schedule('Flag Down! Player 2 Wins!')
-            view.remove_subview(player1button)
-            view.remove_subview(player2button)
+def countdown():
+    global t
+    if t > 0:
+        l1.config(text=t)
+        t = t-1
+        l1.after(1000, countdown)
+    elif t == 0:
+        print('end')
+        l1.config('Goo')
 
 
-@ui.in_background
-def timer2(sender):
-    global time4
-    global time5
-    global time6
-    global tap1
-    global tap2
+root = Tk()
 
-    if tap2 == False:
-        if time6 == 0 & (time5 > 0 | time4 > 0):
-            time6 = 9
-            if time5 == 0:
-                time4 -= 1
-                time5 = 5
-            else:
-                time5 -= 1
-        else:
-            time6 -= 1
-            time.sleep(1)
-            player1button.title = str(time4) + ':' + str(time5) + str(time6)
-            timer2(sender)
-        if time4 == time5 == time6 == 0:
-            notification.schedule('Flag Down! Player 1 Wins!')
-            view.remove_subview(player1button)
-            view.remove_subview(player2button)
+root.geometry('180x150')
+
+l1 = Label(root, font='times 20')
+l1.grid(row=1, column=2)
+
+times = StringVar()
+e1 = Entry(root, textvariable=times)
+e1.grid(row=3, column=2)
 
 
-player1button.action = timer2
-player1button.action = player1tap
-player2button.action = timer1
-player2button.action = player2tap
+b1 = Button(root, text='SET', width=20, command=set_timer)
+b1.grid(row=4, column=2, padx=20)
 
-view.add_subview(quitbutton)
-view.add_subview(player1button)
-view.present()
+b2 = Button(root, text='START', width=20, command=countdown)
+b2.grid(row=6, column=2, padx=20)
+
+
+root.mainloop()
