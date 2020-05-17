d6d6a4895071edf65edab134fff4b6efe019ffae
Tareq Anuar <MTareqAzim@gmail.com>
2020-05-17 11:06:47 +0800

    WinsBreakdownFrame is now a Frame.

--- sv_tracker/gui/graphs/wins_breakdown_frame.py
+++ sv_tracker/gui/graphs/wins_breakdown_frame.py
@@ -4,16 +4,16 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import sv_tracker.helper_functions as hf
 
 
-class WinsBreakdownFrame:
-    def __init__(self, parent, deck):
+class WinsBreakdownFrame(Frame):
+    def __init__(self, master, deck):
         self.deck = deck
         self.wins_breakdown = deck.wins_breakdown()
-        self.frame = Frame(parent, width=300, height=225)
+        super().__init__(master, width=300, height=225)
         self.wins_chart = self.generate_graph()
 
     def generate_graph(self):
         if self.wins_breakdown == [0, 0, 0, 0, 0, 0, 0, 0]:
-            wins_chart = Label(self.frame, text="NO DATA", font='Times 36 bold')
+            wins_chart = Label(self, text="NO DATA", font='Times 36 bold')
             wins_chart.pack(fill=BOTH, expand=TRUE)
             return wins_chart
 
@@ -27,15 +27,17 @@ class WinsBreakdownFrame:
                autopct=lambda p: '{:.0f}'.format(p * total / 100))
         ax.set_title('Wins Breakdown By Class')
 
-        background = hf.rgb_to_hex(self.frame.winfo_rgb(self.frame['bg']))
+        background = hf.rgb_to_hex(self.winfo_rgb(self['bg']))
         fig.patch.set_facecolor(background)
 
-        wins_chart = FigureCanvasTkAgg(fig, self.frame)
+        wins_chart = FigureCanvasTkAgg(fig, self)
         wins_chart.get_tk_widget().pack(fill=BOTH)
         return wins_chart
 
     def destroy(self):
         if type(self.wins_chart) is Label:
+            super().destroy()
             return
 
         plt.close(self.wins_chart.figure)
+        super().destroy()
