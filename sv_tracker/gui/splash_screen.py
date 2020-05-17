7885f6d6bba4779db47524ed4b687a44c217f596
Tareq Anuar <MTareqAzim@gmail.com>
2020-05-17 10:39:20 +0800

    SplashScreen is now a Toplevel.

--- sv_tracker/gui/splash_screen.py
+++ sv_tracker/gui/splash_screen.py
@@ -1,23 +1,23 @@
 from tkinter import *
 
 
-class SplashScreen:
+class SplashScreen(Toplevel):
     def __init__(self, master):
-        self.toplevel = Toplevel(master)
-        self.toplevel.wm_overrideredirect(True)
+        super().__init__(master)
+        self.wm_overrideredirect(True)
 
-        self.label = Label(self.toplevel, text='LOADING', font='fixedsys 20 bold')
+        self.label = Label(self, text='LOADING', font='fixedsys 20 bold')
         self.label.pack()
 
-        width = self.toplevel.winfo_reqwidth()
-        height = self.toplevel.winfo_reqheight()
+        width = self.winfo_reqwidth()
+        height = self.winfo_reqheight()
 
         top_left_x = int(master.winfo_screenwidth()/2 - width/2)
         top_left_y = int(master.winfo_screenheight()/2 - height/2)
 
-        self.toplevel.geometry("+{}+{}".format(top_left_x, top_left_y))
+        self.geometry("+{}+{}".format(top_left_x, top_left_y))
 
-        self.toplevel.update()
+        self.update()
 
     def destroy(self):
-        self.toplevel.destroy()
+        super().destroy()
