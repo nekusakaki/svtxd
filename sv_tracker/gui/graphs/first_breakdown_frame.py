64eb285957c0008359202f4ceba3362c9f31aa42
Tareq Anuar <MTareqAzim@gmail.com>
2020-05-17 10:55:43 +0800

    FirstBreakdownFrame is now a Frame.

--- sv_tracker/gui/graphs/first_breakdown_frame.py
+++ sv_tracker/gui/graphs/first_breakdown_frame.py
@@ -4,16 +4,16 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import sv_tracker.helper_functions as hf
 
 
-class FirstBreakdownFrame:
-    def __init__(self, parent, deck):
+class FirstBreakdownFrame(Frame):
+    def __init__(self, master, deck):
         self.deck = deck
         self.first_breakdown = deck.first_breakdown()
-        self.frame = Frame(parent, width=300, height=225)
+        super().__init__(master, width=300, height=225)
         self.first_chart = self.generate_graph()
 
     def generate_graph(self):
         if self.first_breakdown == [0, 0]:
-            first_chart = Label(self.frame, text="NO DATA", font='Times 36 bold')
+            first_chart = Label(self, text="NO DATA", font='Times 36 bold')
             first_chart.pack(fill=BOTH, expand=TRUE)
             return first_chart
 
@@ -25,15 +25,17 @@ class FirstBreakdownFrame:
         ax.pie(self.first_breakdown, labels=legend, colors=colors, autopct='%1.2f')
         ax.set_title('Going First Breakdown')
 
-        background = hf.rgb_to_hex(self.frame.winfo_rgb(self.frame['bg']))
+        background = hf.rgb_to_hex(self.winfo_rgb(self['bg']))
         fig.patch.set_facecolor(background)
 
-        first_chart = FigureCanvasTkAgg(fig, self.frame)
+        first_chart = FigureCanvasTkAgg(fig, self)
         first_chart.get_tk_widget().pack(fill=BOTH)
         return first_chart
 
     def destroy(self):
         if type(self.first_chart) is Label:
+            super().destroy()
             return
 
         plt.close(self.first_chart.figure)
+        super().destroy()
--- sv_tracker/gui/stats_frame.py
+++ sv_tracker/gui/stats_frame.py
@@ -50,7 +50,7 @@ class StatsFrame(Frame):
         self.match_breakdown_frame.frame.pack(fill=BOTH, expand=True)
         self.wins_breakdown_frame.frame.pack(fill=BOTH, expand=True)
         self.losses_breakdown_frame.frame.pack(fill=BOTH, expand=True)
-        self.first_breakdown_frame.frame.pack(fill=BOTH, expand=True)
+        self.first_breakdown_frame.pack(fill=BOTH, expand=True)
         self.second_breakdown_frame.frame.pack(fill=BOTH, expand=True)
 
         self.match_history_frame.grid(row=2, column=0, sticky=N+E+W+S, pady=5)
@@ -59,7 +59,7 @@ class StatsFrame(Frame):
         self.figures_notebook.add(self.match_breakdown_frame.frame, text='Opponents')
         self.figures_notebook.add(self.wins_breakdown_frame.frame, text='Wins')
         self.figures_notebook.add(self.losses_breakdown_frame.frame, text='Losses')
-        self.figures_notebook.add(self.first_breakdown_frame.frame, text='First')
+        self.figures_notebook.add(self.first_breakdown_frame, text='First')
         self.figures_notebook.add(self.second_breakdown_frame.frame, text='Second')
 
         self.edit_matches.grid(row=3, column=0, sticky=N+E+W+S, padx=5, pady=5)
